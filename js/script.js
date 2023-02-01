48006fccdcb65b76fadfe078df41123ad4029edc
punSavitri <punshakshi@gmail.com>
2023-02-01 15:41:11 +0000

    leaflet map not showing on webpage

--- js/script.js
+++ js/script.js
@@ -1,4 +1,60 @@
+//global variables
+var myMap;
+var lyrOSM;
+var lyrOpenTopo;
+var lyrEsriWorldImagery;
+var baseLayers;
+var overlays;
+var ctrlPan;
+var ctrlEasyButton;
+var ctrlSidebar;
+
 $(document).ready(function () {
+  //create leaflet map object
+  myMap = L.map("map_div", {
+    center: [51.2625, -1.0871],
+    zoom: 13,
+    zoomControl: false,
+  });
+
+  //adding marker to map
+  L.marker([51.2625, -1.0871])
+    .addTo(myMap)
+    .bindPopup("Basingstoke")
+    .openPopup();
+
+  //adding  raster layers as base map
+  lyrOSM = L.tileLayer.provider("OpenStreetMap.Mapnik");
+
+  lyrOpenTopo = L.tileLayer.provider("OpenTopoMap");
+
+  lyrEsriWorldImagery = L.tileLayer
+    .provider("Esri.WorldImagery")
+    .myMap.addLayer(lyrOSM);
+
+  baseLayers = {
+    "OpenStreetMap" : lyrOSM,
+    "OpenTopoMap" : lyrOpenTopo,
+    "Esri.WorldImagery" : lyrEsriWorldImagery,
+  };
+  overlays = {
+    "OpenStreetMap": lyrOSM,
+    "OpenTopoMap": lyrOpenTopo,
+    "Esri.WorldImagery": lyrEsriWorldImagery,
+  };
+
+  L.control.layers(baseLayers, overlays).addTo(myMap);
+
+  //add panControl() method in myMap
+  ctrlPan = L.control.pan();
+  ctrlPan.addTo(myMap);
+
+  //adding easybutton and sidebar toggle()
+  ctrlEasyButton = L.easyButton("fas fa-exchange", function () {
+    ctrlSidebar.toggle();
+  }).addTo(myMap);
+
+  //country list
   $.ajax({
     type: "GET",
     url: "php/getCountryBorders.php",
@@ -6,9 +62,9 @@ $(document).ready(function () {
     success: function (result) {
       console.log(result);
 
-      for (var i = 0; i < result.data.features.length; i++) {
+      for (var i = 0; i < result.data.length; i++) {
         $("#select_country").append(
-          `<option value="${result.data.features[i].properties.iso_a3}">${result.data.features[i].properties.name}</option>`
+          `<option value="${result.data[i].properties.iso_a3}">${result.data[i].properties.name}</option>`
         );
       }
     },
