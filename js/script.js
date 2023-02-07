2ce1d7fc35c6ce1a17757663bce1d0079bdd6791
punSavitri <punshakshi@gmail.com>
2023-02-07 12:25:55 +0000

    remove border

--- js/script.js
+++ js/script.js
@@ -1,86 +1,107 @@
 var myMap;
-$(document)
-  .ready(function () {
-    //dropdown list of countries
-    $.ajax({
-      type: "GET",
-      url: "php/getCountryBorders.php",
-      dataType: "json",
-      success: function (result) {
-        console.log(result);
+var border;
 
-        for (var i = 0; i < result.data.length; i++) {
-          $("#select_country").append(
-            `<option value="${result.data[i].properties.iso_a3}">${result.data[i].properties.name}</option>`
-          );
-        }
-      },
-      error: function (jqXHR, textStatus, errorThrown) {
-        console.log(jqXHR);
-      },
-    });
+$(document).ready(function () {
+  //dropdown list of countries
+  $.ajax({
+    type: "GET",
+    url: "php/getCountryBorders.php",
+    dataType: "json",
+    success: function (result) {
+      console.log(result);
 
-    // //create leaflet map object
-    var myMap = L.map("map_div").setView([0, 0], 13);
-
-    // //adding marker to map
-
-    // //adding  raster layers as base map
-    var lyrOsm = L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
-      maxZoom: 13,
-      attribution:
-        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-    });
-    myMap.addLayer(lyrOsm);
-
-    // //add panControl() method in myMap
-    var ctrlPan = L.control.pan().addTo(myMap);
-
-    var objBaseLayers = {
-      OpenStreetMap: lyrOsm,
-    };
-
-    var objOverlays = {
-      OpenStreetMap: lyrOsm,
-    };
-
-    L.control.layers(objBaseLayers, objOverlays).addTo(myMap);
-
-    //ploting border to selected country
-    $("#select_country").change(function () {
-      $.ajax({
-        url: "php/highlightCountryBorder.php",
-        type: "GET",
-        dataType: "json",
-        success: function (response) {
-          console.log(response);
-          L.geoJson(
-            response,
-            { onEachFeature: forEachFeature },
-            { style: myStyle }
-          ).addTo(myMap);
-        },
-        error: function (error) {
-          console.log(error);
-        },
-      });
-      myMap.fitBounds(response.getBounds());
-    });
-
-    function forEachFeature(feature, layer) {
-      if (feature.geometry.type === "MultiPolygon") {
-        layer.bindPopup(feature.geometry.coordinates.join(""));
+      for (var i = 0; i < result.data.length; i++) {
+        $("#select_country").append(
+          `<option value="${result.data[i].properties.iso_a2}">${result.data[i].properties.name}</option>`
+        );
       }
-    }
-  })
-  
-//set style for layer
-function myStyle(feature) {
-  return {
-    fillColor: "yello",
-    color: "green",
-    weight: 1,
-    opacity: 1,
-    fillOpacity: 0.7,
+    },
+    error: function (jqXHR, textStatus, errorThrown) {
+      console.log(jqXHR);
+    },
+  });
+
+  // //create leaflet map object
+  var myMap = L.map("map_div").setView([51.2665, 1.0924], 8);
+
+  // //adding  tile layer as base map
+  var lyrOsm = L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+    maxZoom: 13,
+    attribution:
+      '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+  });
+  myMap.addLayer(lyrOsm);
+
+  // // adding customize marker to map
+  // var orangeLeafMarker = L.icon({
+  //   iconUrl: "leaf-orange.png",
+  //   shadowUrl: "leaf-shadow.png",
+  //   iconSize: [38, 95],
+  //   shadowSize: [50, 64],
+  //   iconAnchor: [22, 94],
+  //   shadowAnchor: [4, 62],
+  //   popupAnchor: [-3, -76],
+  // });
+  // var marker = L.maker([51.2665, 1.0924], { icon: orangeLeafMarker });
+  // marker.addTo(myMap);
+
+  // //add panControl() method in myMap
+  var ctrlPan = L.control.pan().addTo(myMap);
+
+  var objBaseLayers = {
+    OpenStreetMap: lyrOsm,
   };
-}
+
+  var objOverlays = {
+    OpenStreetMap: lyrOsm,
+  };
+
+  L.control.layers(objBaseLayers, objOverlays).addTo(myMap);
+
+  //ploting border to selected country
+  $("#select_country").change(function () {
+    $.ajax({
+      url: "php/highlightCountryBorder.php",
+      type: "GET",
+      dataType: "json",
+      data: {
+        iso: $("#select_country").val(),
+      },
+      success: function (data) {
+        console.log(data);
+
+        //remove border layer
+        if (data.data.name == "ok") {
+          if (myMap.hasLayer(border)) {
+            myMap.removeLayer(border);
+          }
+        }
+        //set style
+        var border = L.geoJSON(
+          data.data,
+          {
+            style: function (feature) {
+              return {
+                color: "green",
+                weight: 1,
+                opacity: 1,
+                fillOpacity: 0.3,
+              };
+            },
+          },
+          {
+            //bind country name when we click on country
+            onEachFeature: function (feature, layer) {
+              layer.bindPopup(feature.data.properties.name);
+            },
+          }
+        ).addTo(myMap);
+
+        myMap.fitBounds(border.getBounds());
+      },
+      error: function (error) {
+        console.log(error);
+      },
+    });
+  });
+});
--- php/highlightCountryBorder.php
+++ php/highlightCountryBorder.php
@@ -5,15 +5,26 @@ error_reporting(E_ALL);
 
 $executionStartTime = microtime(true);
 
-$result = file_get_contents('countryBorders.geo.json');
+$result = file_get_contents("countryBorders.geo.json");
 
 $decode = json_decode($result, true);
-print_r($decode);
+
+// print_r($decode);
+$countryBorder;
+
+for ($i = 0; $i < count($decode['features']); $i++) {
+  
+  if ($decode['features'][$i]['properties']['iso_a2'] == $_REQUEST['iso']) {
+    $countryBorder = $decode['features'][$i]['geometry'];
+  }
+};
 $output['status']['code'] = "200";
 $output['status']['name'] = "ok";
 $output['status']['description'] = "success";
 $output['status']['returnedIn'] = intval((microtime(true) - $executionStartTime) * 1000) . " ms";
-$output['data'] = $decode;
+
+$output['data'] = $countryBorder;
+
 header('Content-Type: application/json; charset=UTF-8');
 
 echo json_encode($output);
