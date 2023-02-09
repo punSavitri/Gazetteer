254af0f76999bc1c1ef0e8db5ad76628965e49a3
punSavitri <punshakshi@gmail.com>
2023-02-09 12:53:03 +0000

    implement weatherapi and countryflagsapi to geojson file

--- js/script.js
+++ js/script.js
@@ -23,9 +23,9 @@ $(document).ready(function () {
   });
 
   // //create leaflet map object
-  var myMap = L.map("map_div").setView([51.2665, 1.0924], 8);
+  var myMap = L.map("map_div").setView([51.2665, 1.0924], 10);
 
-  // //adding  tile layer as base map
+  // //Basemap
   var lyrOsm = L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
     maxZoom: 13,
     attribution:
@@ -35,8 +35,8 @@ $(document).ready(function () {
 
   // // adding customize marker to map
   var orangeLeafMarker = L.icon({
-    iconUrl: "leaf-orange.png",
-    shadowUrl: "leaf-shadow.png",
+    iconUrl: "https://leafletjs.com/examples/custom-icons/leaf-orange.png",
+    shadowUrl: "https://leafletjs.com/examples/custom-icons/leaf-shadow.png",
     iconSize: [38, 95],
     shadowSize: [50, 64],
     iconAnchor: [22, 94],
@@ -44,7 +44,7 @@ $(document).ready(function () {
     popupAnchor: [-3, -76],
   });
 
-  marker = L.maker([51.2665, 1.0924], { icon: orangeLeafMarker });
+  marker = L.marker([51.2665, 1.0924], { icon: orangeLeafMarker });
   marker.addTo(myMap);
 
   // //add panControl() method in myMap
@@ -72,32 +72,55 @@ $(document).ready(function () {
       success: function (data) {
         console.log(data);
 
-        //remove border layer
-        if (data.data.name == "ok") {
-          if (myMap.hasLayer(border)) {
-            myMap.removeLayer(border);
-          }
+        //clear border layer
+        if (border) {
+          border.clearLayers();
         }
-        //set style
-        border = L.geoJSON(
-          data.data,
-          {
-            style: function (feature) {
-              return {
-                color: "green",
-                weight: 1,
-                opacity: 1,
-                fillOpacity: 0.3,
-              };
-            },
-          },
-          {
-            //bind country name when we click on country
-            onEachFeature: function (feature, layer) {
-              layer.bindPopup(feature.data.properties.name);
-            },
-          }
-        ).addTo(myMap);
+        //fetch weather api for country listed in geojson file for current weather
+        border = L.geoJSON(data.data, {
+          onEachFeature: funcForEachFeature,
+          style: funcStyle,
+        }).addTo(myMap);
+
+        function funcForEachFeature(feature, layer) {
+          layer.on("click", function (e) {
+            fetch(
+              "http://api.weatherapi.com/v1/current.json?key=39485f949c7847c58a2110603230902&q=" +
+                feature.properties.name +
+                "&aqi=no"
+            )
+              .then((response) => response.json())
+              .then((data) => {
+                var weather =
+                  "<div><p><b>Weather:</b>" +
+                  data.current.condition.text +
+                  data.current.condition.icon +
+                  "<br><b>Humidity:</b>" +
+                  data.current.humidity +
+                  "<br><b>Temperature in &deg;C:</b>" +
+                  data.current.temp_c +
+                  "<p></div>";
+                layer.bindPopup(
+                  '<img src="https://countryflagsapi.com/png/' +
+                    feature.properties.name +
+                    '"width="30px" height="20px"/><h2>' +
+                    feature.properties.name +
+                    "</h2>" +
+                    weather
+                );
+              })
+              .catch((error) => console.log(error));
+          });
+        }
+        //set style for feature
+        function funcStyle(feature) {
+          return {
+            color: "green",
+            weight: 1,
+            opacity: 1,
+            fillOpacity: 0.3,
+          };
+        }
 
         myMap.fitBounds(border.getBounds());
       },
